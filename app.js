9789ad5870c94f20f2202fb44528fe5e99c555f5
leonardobussi <leonardobezerrabussy@gmail.com>
2020-07-14 14:01:34 -0300

    Receiving and validating data from

--- app.js
+++ app.js
@@ -3,4 +3,5 @@ const app = require('./config/server')
 app.listen(80, function(){
     console.log('------------------------------')
     console.log('servidor rodando na porta 80')
+    console.log('------------------------------')
 })
--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,18 @@
+const { validationResult } = require("express-validator/check");
+
 module.exports.iniciaChat = function (application, req, res){
+
+    const dadosForm = req.body;
+
+    req.assert('apelido', 'Nome ou apelido é obrigatorio').notEmpty();
+    req.assert('apelido', 'Nome ou apelido obrigatoriamente deve conter de 3 a 15 caracteres').len(3,15);
+
+    const erros = req.validationErrors();
+
+    if(erros){
+        res.render("index", {validacao: erros}) 
+        return ;
+    }
+
     res.render('chat')
 }
